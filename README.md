eba8957ca14c9be9ed7c2159805b0006c9e542ac
Ali Ezadkhaha <hodhod212@yahoo.com>
2020-08-18 10:13:10 +0200

    Created using information in README.md

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# blog
-blog
+Hur man startar hemsidan:
+1- Man måste ha node version 14.5.0 eller högre installerat på datorn.
+2- Ladda ner nodemon globalt eller som devDependencies på datorn.
+3- Ladda ner mongodb på datorn.
+3- npm i
+4- npm start
+
+Git repository: https://github.com/hodhod212/blog.git
+
+Källor:
+1- https://www.youtube.com/watch?v=SBvmnHTQIPY
+2- https://stackoverflow.com/
+3- https://nodejs.org/en/docs/
+4- https://www.youtube.com/watch?v=OIBIXYLJjsI&list=PL4cUxeGkcC9jsz4LDYc6kv3ymONOKxwBU&index=2
+5- https://www.youtube.com/watch?v=MDENx86nK4k
+6- https://www.youtube.com/watch?v=1NrHkjlWVhM
+
+Man ska först skapa en inlogning och logga in , sen man kan komma i sidan , med en blog där man kan skapa, radera eller redigera. Man får också kolla på weather i alla städer i världen genom att skriva namnet på staden eller landet. Samt man får en random skapare av sidan.
+Om man laddar om sidan, information om blogget kommer att sparas i databasen, men inloggning uppgifter kommer att försvinna och man måste skapa en ny inloggning, efterson den sparas inte i databasen.
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import dotenv from "dotenv";
 if (process.env.NODE_ENV !== "production") {
   dotenv.config();
 }
+import axios from "axios";
+
 import mongoose from "mongoose";
 import articleRouter from "./routes/article.js";
 import Article from "./models/article.js";
@@ -14,6 +16,8 @@ import session from "express-session";
 import passport from "passport";
 import methodOverride from "method-override";
 import initializePassport from "./passport-config.js";
+const DARKSKY_API_KEY = process.env.DARKSKY_API_KEY;
+app.use(express.json());
 mongoose
   .connect("mongodb://127.0.0.1:27017/blog", {
     useNewUrlParser: true,
@@ -47,6 +51,13 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(methodOverride("_method"));
+app.post("/weather", (req, res) => {
+  const url = `https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${req.body.latitude},${req.body.longitude}?units=auto`;
+  axios({
+    url: url,
+    responseType: "json",
+  }).then((data) => res.json(data.data.currently));
+});
 app.get("/", checkAuthenticated, async (req, res) => {
   const articles = await Article.find().sort({ createdAt: "desc" });
   res.render("articles/index.ejs", { name: req.user.name, articles: articles });
@@ -86,6 +97,7 @@ app.delete("/logout", (req, res) => {
   req.logOut();
   res.redirect("/login");
 });
+
 function checkAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
@@ -100,4 +112,5 @@ function checkNotAuthenticated(req, res, next) {
   }
   next();
 }
+
 app.listen(5002);
